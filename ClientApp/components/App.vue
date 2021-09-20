307646e8a42900d98e67dd841f27373228b9215b
Samuel Clark <clarks21@email.franklin.edu>
2021-09-20 13:05:28 -0400

    Initial Commit
    Contains Initial Project Setup

---
+++ ClientApp/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1> {{  message }} </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    data() {
+        return {
+            message: "Vue.js and asp.net core 3.1 integration setup"
+        }
+    }
+}
+</script>
